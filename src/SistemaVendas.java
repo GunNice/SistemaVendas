c92fad30c114fb20467109eb817ff379f178b9a8
GunNice <gunnicerca@gmail.com>
2023-06-10 13:39:18 -0300

    .

---
+++ src/SistemaVendas.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class SistemaVendas {
+    public static void main(String[] args) throws Exception {
+
+    
+        
+        System.out.println("===============================");
+        System.out.println("=      \u001B[3mSistema de Vendas      =");
+        System.out.println("= \u001B[3mselecione a opção desejada  =");
+        System.out.println("===============================\n\n");
+        System.out.println("1 - Incluir produto");
+        System.out.println("2 - Consultar produto");
+        System.out.println("3 - Listagem de produtos");
+        System.out.println("4 - Vendas por período - detalhado");
+        System.out.println("5 - Realizar venda");
+        System.out.println("0 - Sair\n\n");
+
+      
+    }
+      
+       
+}
